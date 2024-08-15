73da068be9428ca0ad79162dd477d5b3914074eb
APJSIR <141132387+APJSIR@users.noreply.github.com>
2024-08-15 13:50:33 +0000

    containet config done

---
+++ o1_basic/01_var.js
@@ -0,0 +1,7 @@
+const accountId = 1445553
+let accountEmail = "abcd@gmail.com"
+var accountPassward = "12345"
+accountCity = "wesr bengal"
+//accountId =2
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassward,accountCity])
