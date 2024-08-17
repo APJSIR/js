28edc695499f462e6a4e9c719102e7dbb799e001
APJSIR <141132387+APJSIR@users.noreply.github.com>
2024-08-17 12:09:51 +0000

    conversion

---
+++ o1_basic/03_conversionData.js
@@ -0,0 +1,23 @@
+let score = "33abc"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+let valueNumber = Number(score)
+//console.log(typeof valueNumber);
+//console.log( valueNumber);
+//"33"=> 33
+// "33abc" => NaN
+//true => 1; false = 0;
+
+let isLoggedIn = "rabin"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//"" => false
+// "name " => true
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
