53b75afd1523670dd2c99601d84f34eb6f8d5868
APJSIR <141132387+APJSIR@users.noreply.github.com>
2024-08-18 15:37:03 +0000

    18/08/2024

--- o1_basic/03_conversionData.js
+++ o1_basic/03_conversionData.js
@@ -1,23 +1,44 @@
-let score = "33abc"
-//console.log(typeof score);
-//console.log(typeof(score));
+// let score = "33abc"
+// //console.log(typeof score);
+// //console.log(typeof(score));
 
 
-let valueNumber = Number(score)
-//console.log(typeof valueNumber);
-//console.log( valueNumber);
-//"33"=> 33
-// "33abc" => NaN
-//true => 1; false = 0;
+// let valueNumber = Number(score)
+// //console.log(typeof valueNumber);
+// //console.log( valueNumber);
+// //"33"=> 33
+// // "33abc" => NaN
+// //true => 1; false = 0;
 
-let isLoggedIn = "rabin"
+// let isLoggedIn = "rabin"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
-//"" => false
-// "name " => true
-let someNumber = 33;
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// //"" => false
+// // "name " => true
+// let someNumber = 33;
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+// 18/8/24
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2-2)
+// console.log(2+2)
+// console.log(282)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 ="hello"
+let str2 = "ASD"
+let str3 = str1 +str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1+"2");
+console.log("1"+2+2);
+//post and pre read on the mdn website
