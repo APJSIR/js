77c10b105c5f7f23ff7275e3310753f683b05a93
APJSIR <141132387+APJSIR@users.noreply.github.com>
2024-08-22 02:48:47 +0000

    more learn

---
+++ o1_basic/datatypes_summary.js
@@ -0,0 +1,24 @@
+//#Primitive 
+
+// 7 types: string,number,boolean, null,undefined,symbol,bigint
+
+const score = 100
+
+const scoreVal = 100.3
+
+const isLoggedIn = false
+const outsideTemp= null
+
+const id = Symbol('123')
+const anotherId = Symbol('12345')
+
+console.log(id === anotherId);
+const heros = ["shaktiman", "naagraj", "doga"];
+let objects = {
+    name: "sam",
+    age: 22,
+}
+
+//Reference(non primitive)
+
+// array, objects, functions
