a60c134c67a32897b328a838fa8f10b20b87cb84
APJSIR <141132387+APJSIR@users.noreply.github.com>
2024-08-28 12:20:08 +0000

    NEXT DAY

---
+++ o1_basic/06_num_and_math.js
@@ -0,0 +1,14 @@
+const score = 400;
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));
+
+const otherNumber = 23.1234 
+
+console.log(otherNumber.toPrecision(3))
+
+const hundreads = 1000000
+console.log(hundreads.toLocaleString('en-IN'));
