a0b42b347dbc1574255916f3dcbbec327c248787
APJSIR <141132387+APJSIR@users.noreply.github.com>
2024-10-16 14:10:13 +0000

    16/10/24

---
+++ controlflow/one.js
@@ -0,0 +1,20 @@
+// const temp = 45
+
+// if(temp === 40){
+//     console.log("epual to 40");
+// }else{
+//     console.log("greater than 40");
+// }
+
+
+
+// const sc = 200;
+
+// if (sc>100) {
+//     let pw = "fly"
+//     console.log(`User power: ${pw}`);    
+// }
+// console.log(`User power: ${pw}`);
+
+
+ 
---
+++ controlflow/switch.js
@@ -0,0 +1,19 @@
+ const month = 3
+
+ switch (month) {
+    case 1:
+        console.log("J");
+        break;
+    case 2:
+        console.log("F");
+        break;
+    case 3:
+        console.log("M");
+        break;
+    case 4:
+        console.log("A");
+        break;
+ 
+    default:
+        break;
+ }
